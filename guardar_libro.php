dba6ddf56b4b88f3b7ff0b4b2269e3412ae6560a
sfcontrerassepulveda-cloud <sf.contrerassepulveda@unicienciabga.edu.co>
2026-05-06 22:48:21 -0500

    Update guardar_libro.php

--- guardar_libro.php
+++ guardar_libro.php
@@ -6,7 +6,7 @@ if (!isset($_SESSION['id']) || $_SESSION['tipo_usuario'] != 'bibliotecario') {
     exit;
 }
 
-$conexion = pg_connect("host=localhost dbname=biblioteca2025 user=postgres password=1234");
+$conexion = pg_connect("postgresql://postgres:TLlzVltHnMUWvkOawjQeRKjmQBGBnjzW@trolley.proxy.rlwy.net:16774/railway");
 
 if (!$conexion) {
     die("Error al conectar a la base de datos.");

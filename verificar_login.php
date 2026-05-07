75ae0b6ab51ad8ace1312ff408e8e97de96c14c5
sfcontrerassepulveda-cloud <sf.contrerassepulveda@unicienciabga.edu.co>
2026-05-06 23:40:01 -0500

    corregir verificar login

--- verificar_login.php
+++ verificar_login.php
@@ -1,7 +1,7 @@
 <?php
 session_start(); 
 
-$conexion = pg_connect("postgresql://postgres:TLlzVltHnMUWvkOawjQeRKjmQBGBnjzW@postgres.railway.internal:5432/railway");
+$conexion = pg_connect("postgresql://postgres:TLlzVltHnMUWvkOawjQeRKjmQBGBnjzW@postgres.railway.internal:16774./railway");
 if (!$conexion) {
     die("Error al conectar a la base de datos.");
 }

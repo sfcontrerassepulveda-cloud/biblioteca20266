0e99778fdc8194d5b8044964de999486f7d46e35
sfcontrerassepulveda-cloud <sf.contrerassepulveda@unicienciabga.edu.co>
2026-05-06 21:47:42 -0500

    Update guardar_usuario.php

--- guardar_usuario.php
+++ guardar_usuario.php
@@ -1,6 +1,6 @@
 <?php
 // Conexión a la base de datos
-$conexion = pg_connect("host=localhost dbname=biblioteca2025 user=postgres password=1234");
+$conexion = pg_connect("postgresql://postgres:TLlzVltHnMUWvkOawjQeRKjmQBGBnjzW@postgres.railway.internal:5432/railway");
 
 if (!$conexion) {
     die("Error al conectar a la base de datos.");

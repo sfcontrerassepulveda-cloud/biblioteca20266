1d7651b16c78d1206cad8c3f8d95d2db98075100
sfcontrerassepulveda-cloud <sf.contrerassepulveda@unicienciabga.edu.co>
2026-05-06 22:31:59 -0500

    Update guardar_usuario.php

--- guardar_usuario.php
+++ guardar_usuario.php
@@ -1,6 +1,6 @@
 <?php
 // Conexión a la base de datos
-$conexion = pg_connect("postgresql://postgres:TLlzVltHnMUWvkOawjQeRKjmQBGBnjzW@postgres.railway.internal:5432/railway");
+$conexion = pg_connect("postgresql://postgres:TLlzVltHnMUWvkOawjQeRKjmQBGBnjzW@trolley.proxy.rlwy.net:16774/railway");
 
 if (!$conexion) {
     die("Error al conectar a la base de datos.");

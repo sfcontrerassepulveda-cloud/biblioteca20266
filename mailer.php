9b996a7bbd438e8e7b676e1189b4c800444ae46c
sfcontrerassepulveda-cloud <sf.contrerassepulveda@unicienciabga.edu.co>
2026-05-06 22:46:11 -0500

    Update mailer.php

--- mailer.php
+++ mailer.php
@@ -6,7 +6,7 @@ use PHPMailer\PHPMailer\Exception;
 session_start();
 
 // Conexión a la base de datos
-$conexion = pg_connect("host=localhost dbname=biblioteca2025 user=postgres password=1234");
+$conexion = pg_connect("postgresql://postgres:TLlzVltHnMUWvkOawjQeRKjmQBGBnjzW@trolley.proxy.rlwy.net:16774/railway");
 if (!$conexion) {
     die("Error al conectar a la base de datos.");
 }

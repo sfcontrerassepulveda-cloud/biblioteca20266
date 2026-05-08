79233dd5aac88c3cf02775c51705dd5ed76df65f
sfcontrerassepulveda-cloud <sf.contrerassepulveda@unicienciabga.edu.co>
2026-05-07 21:56:24 -0500

    Update panel_prestamo.php

--- panel_prestamo.php
+++ panel_prestamo.php
@@ -190,7 +190,7 @@ $resultado = pg_query($conexion, $query);
        
         </table>
 
-        <a href="./" class="btn-volver">Volver al inicio</a>
+        <a href="./biblioteca_inicio" class="btn-volver">Volver al inicio</a>
     </div>
 </body>
 </html>

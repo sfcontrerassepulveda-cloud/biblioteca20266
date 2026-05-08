79a818ecbe350e7cc0bbd9464abf4b997e1f88c1
sfcontrerassepulveda-cloud <sf.contrerassepulveda@unicienciabga.edu.co>
2026-05-07 22:03:04 -0500

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -5,7 +5,7 @@ $user = "postgres";
 $password = "TLlzVltHnMUWvkOawjQeRKjmQBGBnjzW"; // Cambia esto si tu contraseña es diferente
 
 try {
-    $conexion = new PDO("pgsql:host=$host;dbname=$dbname", $user, $password,sslmode=require);
+ $conexion = new PDO("pgsql:host=$host;dbname=$dbname; sslmode=require", $user, $password);
     $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     echo "Error al conectar con la base de datos: " . $e->getMessage();

31e55b418acfa421cda747f895c15d46e08ac9a5
sfcontrerassepulveda-cloud <sf.contrerassepulveda@unicienciabga.edu.co>
2026-05-21 21:43:58 -0500

    Update registro_lector.php

--- registro_lector.php
+++ registro_lector.php
@@ -55,15 +55,13 @@
                 <div class="input_item">
                     <label for="identificacion">Número de Identificación:</label>
                     <input type="text" name="identificacion" id="identificacion" required data-ayuda="Solo números, mínimo 10 dígitos.">
-              <?php
-if(isset($_SESSION['mensaje_identificacion'])){
-    echo '<p id="error_id" style="color:red">'
-        . $_SESSION['mensaje_identificacion'] .
-        '</p>';
+              <?php  session_start();
+                    if(isset($_SESSION['mensaje_identificacion'])){
+                       echo '<p id="error_id" style="color:red">'.$_SESSION['mensaje_identificacion'].'</p>';
 
-    unset($_SESSION['mensaje_identificacion']);
-}
-?>
+                        unset($_SESSION['mensaje_identificacion']);
+                    }
+                    ?>
                     <div class="ayuda"></div>
                    
                 </div>

9a0a4956e6976123e5a033db7baa90af6efd8c01
sfcontrerassepulveda-cloud <sf.contrerassepulveda@unicienciabga.edu.co>
2026-05-21 22:00:54 -0500

    Update admin_usuarios.php

--- admin_usuarios.php
+++ admin_usuarios.php
@@ -1,6 +1,6 @@
 <?php
 // Conexión a la base de datos
-$conexion = pg_connect("postgresql://postgres:TLlzVltHnMUWvkOawjQeRKjmQBGBnjzW@trolley.proxy.rlwy.net:16774/railway");
+$conexion = pg_connect("host=localhost dbname=biblioteca2025 user=postgres password=1234");
 
 if (!$conexion) {
     die("Error al conectar a la base de datos.");
